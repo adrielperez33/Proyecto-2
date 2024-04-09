7edda7d4d83f4c5e15a479f9965083b05dedc2a9
adriel <adrielperez227@gmail.com>
2024-04-09 12:40:51 -0300

    primer homework

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,1 +1,35 @@
-console.log(tempData);
+
+const pelis = document.getElementById("tarjetapelicula")
+
+tempData.forEach((peliculas) => {
+    const contenedor = document.createElement("div");
+    contenedor.classList.add("peliculas")
+    
+    const titulo = document.createElement("a")
+    titulo.href = ("#")
+    titulo.innerHTML = peliculas.title;
+    
+    const poster = document.createElement("img")
+    poster.src = peliculas.poster;
+    
+    const director = document.createElement("p")
+    director.innerHTML = peliculas.director;
+    
+    const duracion = document.createElement("p")
+    duracion.innerHTML = peliculas.duration;
+    
+    const genero = document.createElement("p")
+    genero.innerHTML = peliculas.genre;
+    
+    const rate = document.createElement("p")
+    rate.innerHTML = peliculas.rate;
+    
+    const ano = document.createElement("p")
+    ano.innerHTML = peliculas.year;
+    
+    
+    contenedor.append(titulo,poster,director,duracion,genero,ano,rate)
+    
+    pelis.appendChild(contenedor);
+    
+})
