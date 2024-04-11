a40faa38b7dbaa7a3fd4f51e269142a203e11e65
adriel <adrielperez227@gmail.com>
2024-04-11 10:03:33 -0300

    tercer commit

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -29,65 +29,91 @@ class Repository{
 
 const k = new Repository;
 
-function creador(parametros) {
-    
-    console.log(parametros);
-    const {title, year, director, duration, genre, rate, poster} = parametros
-    
-    
-    const contenedor = document.createElement("div");
-    contenedor.classList.add("peliculas")
-    
-    const titulo = document.createElement("a")
-    titulo.href = ("#")
-    titulo.innerHTML = title;
-    
-    const poste = document.createElement("img")
-    poste.src = poster;
-    
-    const directorr = document.createElement("p")
-    directorr.innerHTML = director;
-    
-    const duracion = document.createElement("p")
-    duracion.innerHTML = duration;
-    
-    const genero = document.createElement("p")
-    genero.innerHTML = genre;
-    
-    const rates = document.createElement("p")
-    rates.innerHTML = rate;
-    
-    const ano = document.createElement("p")
-    ano.innerHTML = year;
-    
-    
-    contenedor.append(titulo,ano,directorr,duracion,genero,rates, poste)
-    
-    k.agregarAlArreglo(contenedor);
-    
-}
-
-
 function bucle() {
-
     $.get("https://students-api.up.railway.app/movies", (info) => {
-        info.forEach((pelicula) => {
-            creador(pelicula);
+        const peliculas = info.map((pelicula) => {
+            return creador(pelicula);
         });
-        mostrarPeliculas();
+        mostrarPeliculas(peliculas);
     });
 }
 
-
-function mostrarPeliculas() {
-    const peliculas = k.mostrarArreglo();
+function mostrarPeliculas(peliculas) {
     const pelis = document.getElementById("tarjetapelicula");
-    
-    // pelis.innerHTML = "";
     
     peliculas.forEach((pelicula) => {
         pelis.appendChild(pelicula);
     });
 }
 
+function bucle() {
+    $.get("https://students-api.up.railway.app/movies", (info) => {
+        const peliculas = info.map((pelicula) => {
+            return creador(pelicula);
+        });
+        mostrarPeliculas(peliculas);
+    });
+}
+
+function mostrarPeliculas(peliculas) {
+    const pelis = document.querySelector("#tarjetapelicula .row");
+  
+    peliculas.forEach((pelicula) => {
+      const columna = document.createElement("div");
+      columna.classList.add("col-md-4"); 
+      columna.appendChild(pelicula);
+      pelis.appendChild(columna);
+    });
+  }
+  
+
+function creador(parametros) {
+    const { title, year, director, duration, genre, rate, poster } = parametros;
+
+    const contenedor = document.createElement("div");
+    contenedor.classList.add("card");
+    contenedor.style.width = "18rem";
+
+    const poste = document.createElement("img");
+    poste.classList.add("card-img-top");
+    poste.src = poster;
+    poste.alt = "Poster";
+
+    const cuerpo = document.createElement("div");
+    cuerpo.classList.add("card-body");
+
+    const titulo = document.createElement("a");
+    titulo.classList.add("card-title");
+    titulo.href = "#";
+    titulo.innerHTML = title;
+
+    const directorr = document.createElement("p");
+    directorr.classList.add("card-text");
+    directorr.innerHTML = `director: ${director}`;
+
+    const duracion = document.createElement("p");
+    duracion.classList.add("card-text");
+    duracion.innerHTML = `duracion: ${duration}`;
+
+    const genero = document.createElement("p");
+    genero.classList.add("card-text");
+    genero.innerHTML = `generos: ${genre.join(", ")}`;
+
+    const rates = document.createElement("p");
+    rates.classList.add("card-text");
+    rates.innerHTML = `rate: ${rate}`;
+
+    const ano = document.createElement("p");
+    ano.classList.add("card-text");
+    ano.innerHTML = `año: ${year}`;
+
+    cuerpo.append(titulo, directorr, duracion, genero, rates, ano);
+    
+    contenedor.append(poste, cuerpo);
+
+    return contenedor;
+}
+
+
 bucle();
+
